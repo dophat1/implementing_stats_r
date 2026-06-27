8a814fa4be145dba9d48cc8028f99f25b83057a0
dophat1 <dophat2403@gmail.com>
2026-06-27 23:58:49 +0200

    Add score function of poisson

--- poisson.R
+++ poisson.R
@@ -5,3 +5,10 @@ log_lik_poisson <- function(X, y, beta){
   ll_poisson <- sum(y * eta - muy - lfactorial(y))
   return(ll_poisson)
 }
+
+score_poisson <- function(X,y,beta){
+  eta <- X %*% beta
+  muy <- exp(eta)
+  score <- t(X) %*% (y - muy)
+  return(score)
+}
